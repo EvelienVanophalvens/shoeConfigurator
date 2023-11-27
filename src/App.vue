3c387503596f45d60788447d22384229c4cb8c97
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2023-11-27 08:17:59 +0100

    Feat: add Three.js canvas

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <script setup>
+import Shoe from './components/shoe.vue';
+
 </script>
 
 <template>
+  <div> 
+    logo
+  </div>
+  <Shoe id="shoe" />
 </template>
 
 <style scoped>
+
+
 </style>
