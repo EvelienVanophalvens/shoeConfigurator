addaa375dab11d1e29b2ca069979eeb6f1c04ffa
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2023-11-27 08:19:22 +0100

    Feat: add orbit controlles

--- src/components/Shoe.vue
+++ src/components/Shoe.vue
@@ -1,5 +1,6 @@
 <script setup>
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -9,6 +10,8 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 scene.background = new THREE.Color( 0xffffff );
+
+const controls = new OrbitControls( camera, renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
