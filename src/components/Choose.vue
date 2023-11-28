56712d0411a17db56e6749301506dea3eb77f7c7
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2023-11-28 14:01:49 +0100

    Feat: show right colors

--- src/components/Choose.vue
+++ src/components/Choose.vue
@@ -2,6 +2,9 @@
     import { ref, defineEmits } from 'vue';
     const shoespots = ["Laces", "Outsole", "Midsole", "Outer material", "Mid material", "Inner material", "Tongue"];
     let shoespot = ref(0);
+    let colors = ref(0);
+    let colorOutsole = ["#ffffff", "#000000"];
+
     //define emit function
     const emit = defineEmits(['shoespot-changed']);
     const next = () => {
@@ -12,6 +15,7 @@
         }
         emit('shoespot-changed', shoespot.value);
         document.getElementById("shoespotindicator").innerHTML = shoespots[shoespot.value];
+        updateColors();
     }
     const back = () => {
         if (shoespot.value > 0) {
@@ -21,17 +25,57 @@
         }
         emit('shoespot-changed', shoespot.value);
         document.getElementById("shoespotindicator").innerHTML = shoespots[shoespot.value];
+        updateColors();
     }
+    const updateColors = () => {
+    switch(shoespot.value) {
+        case 0:
+            colors.value = ["#ffffff", "#000000", "#ff0000", "#00ffff", "#00ff00", "#ffff00", "#0000ff", "#000000"];
+            break;
+        case 1:
+            colors.value = ["#ffffff", "#000000"];
+            break;
+        case 2:
+            colors.value = ["#ffffff", "#000000", "#ff0000", "#00ffff", "#00ff00", "#ffff00", "#0000ff", "#000000"];
+            break;
+        case 3:
+            colors.value = ["#ffffff", "#000000", "#ff0000", "#00ffff", "#00ff00", "#ffff00", "#0000ff", "#000000"];
+            break;
+        case 4:
+            colors.value = ["#ffffff", "#000000", "#ff0000", "#00ffff", "#00ff00", "#ffff00", "#0000ff", "#000000"];
+            break;
+        case 5:
+            colors.value = ["#ffffff", "#000000", "#ff0000", "#00ffff", "#00ff00", "#ffff00", "#0000ff", "#000000"];
+            break;
+        case 6:
+            colors.value = ["#ffffff", "#000000", "#ff0000", "#00ffff", "#00ff00", "#ffff00", "#0000ff", "#000000"];
+            break;
+    }
+    }
+    updateColors();
 </script>
 <template>
-    <div class="side"> 
-        <svg class="pointer" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512" @click="next"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
-        <p id="shoespotindicator">Laces</p>
-        <svg class="pointer" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512" @click="back"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
+    <div class="center">
+        <div class="side"> 
+            <svg class="pointer" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512" @click="next"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
+            <p id="shoespotindicator">Laces</p>
+            <svg class="pointer" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512" @click="back"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
+        
+        </div>
+        <div>
+            <ul class="colors"> 
+                <li v-for="c in colors" :style="{ backgroundColor: c }"></li>
+            </ul>
+        </div>
     </div>
 </template>
 <style scoped>
-
+.center {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
 svg {
     width: 2em;
     height: 2em;
@@ -58,5 +102,18 @@ p {
 #shoespotindicator{
     width: 300px;
     text-align: center;
+}
+ul{
+    padding: 0;
+    display: flex;
+    flex-direction: row;
+}
+li {
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    list-style-type: none;
+    margin: 0.5em;
+
 }
 </style>
